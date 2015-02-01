bfe3e3e2e9fbfedc5a909733cfc45a07e94435ac
A-malagon <alejanm09@gmail.com>
2015-02-01 22:48:31 +0100

    Entrega Ejercicio

--- passwd.py
+++ passwd.py
@@ -2,28 +2,37 @@
 # -*- coding: utf-8 -*-
 
 fichero = open("/etc/passwd", "r")
-#Guardamos en una lista cada linea del fichero.
 
+#Guardamos en una lista cada linea del fichero.
 numeroLineas = fichero.readlines()
 print ""
 
 #Creamos un diccionario.
-
 diccionario = {}
 
+contador = 0
 for lineas in numeroLineas:
 	frase = lineas.split(":")
+	if contador == 0:
+		idRoot = frase[0]
+	contador = contador + 1
 	nombreUsuario = frase[0]
 	nombreShell = frase[-1][:-1]
 	diccionario[nombreUsuario] = nombreShell 
 	
-	Idroot = diccionario["root"]
-	print "Tamaño diccionario" + len(diccionario)
-
-	#Identificador de usuario, y shell que utiliza ese identificador de usuario.
-
-	print "Identificador de usuario: " + frase[0] + " =>Shell: " + frase[-1][:-1]
+shellRoot = diccionario["root"]
+longitudDic = str(len(diccionario))
+	
+#Identificador de usuario, y shell que utiliza ese identificador de usuario.
+print "Identificador de usuario: " + idRoot + " => Shell: " + shellRoot
 
 #Número de usuarios para esta máquina.
+print "\n" + "Número de usuarios de la máquina = " + longitudDic
 
-print "\n" + "Número de usuarios de la máquina " + str(len(numeroLineas))
+#Control de excepcion.
+try: 
+	print diccionario["imaginario"]
+except KeyError:
+	print ""
+	print "El identificador de usuario 'imaginario' no existe"
+
